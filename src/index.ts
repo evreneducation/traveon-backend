480aad7a93178693c7f8deb70fbe21b3da3159f5
evreneducation <65123232+evreneducation@users.noreply.github.com>
2025-07-11 13:01:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5000', 'http://localhost:3000', 'http://localhost:5174'],
+  origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5000', 'http://localhost:3000', 'http://localhost:5174', "https://traveon.vercel.app"],
   credentials: true,
 }));
 
